39dd4540172cd4bad6308b3c3808a16d18b5f3a7
Jason Axelson <jason.axelson@gmail.com>
2018-12-02 13:53:48 -1000

    Finished day2 part 1

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -12,7 +12,41 @@ defmodule Day2 do
       :world
 
   """
-  def hello do
-    Advent.input("short.txt")
+  def run do
+    Advent.input("day2_input.txt")
+    |> extract_ids()
+    |> Enum.map(&count_letters/1)
+    |> Enum.map(&two_and_three_count/1)
+    |> checksum()
+  end
+
+  def extract_ids(text), do: String.split(text, "\n", trim: true)
+
+  def count_letters(box_id) do
+    box_id
+    |> String.codepoints()
+    |> Enum.reduce(%{}, fn char, letters ->
+      Map.update(letters, char, 1, &(&1 + 1))
+    end)
+  end
+
+  def two_and_three_count(%{} = letters) do
+    letters
+    |> Enum.reduce({0, 0}, fn
+      {_, 2}, {_, three_count} -> {1, three_count}
+      {_, 3}, {two_count, _} -> {two_count, 1}
+      _, acc -> acc
+    end)
+  end
+
+  def checksum(count_tuples) do
+    {two_count, three_count} =
+      count_tuples
+      |> Enum.reduce({0, 0}, fn
+        {two_count, three_count}, {existing_two_count, existing_three_count} ->
+          {two_count + existing_two_count, three_count + existing_three_count}
+      end)
+
+    two_count * three_count
   end
 end
