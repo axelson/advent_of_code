76d28a5817485e7d6692bc32c59c08c6370d75cc
Jason Axelson <jason.axelson@gmail.com>
2018-12-04 08:42:32 -1000

    wip

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -14,19 +14,50 @@ defmodule Day3 do
     |> length()
   end
 
+  defmodule Result do
+    defstruct [
+      # A map where the key is the coord and the value is the ids that have that coord
+      :claimed,
+      :valid_ids
+    ]
+  end
+
   def part2() do
-    Advent.input("short.txt")
-    |> String.split("\n", trim: true)
-    |> Enum.map(&Claim.parse/1)
+    claims =
+      # Advent.input("short.txt")
+      Advent.input("day3_input.txt")
+      |> String.split("\n", trim: true)
+      |> Enum.map(&Claim.parse/1)
+
+    maybe_valid_ids =
+      claims
+      |> Enum.map(& &1.id)
+      |> MapSet.new()
+
+    claims
     |> Enum.map(&{&1.id, Claim.coords(&1)})
-    |> Enum.reduce({%{}, MapSet.new()}, fn {id, coords}, {claimed, valid_ids} ->
-      {claimed, conflicts} =
-        Enum.reduce(coords, {claimed, []}, fn coord, {claimed, conflicts} ->
-          # Map.get_and_update(claimed, )
+    |> Enum.reduce(%Result{claimed: %{}, valid_ids: maybe_valid_ids}, fn {id, coords}, result ->
+      {conflicting_ids, claimed} =
+        Enum.reduce(coords, {[], result.claimed}, fn coord, {conflicting_ids, claimed} ->
+          {conflicts, claimed} =
+            Map.get_and_update(claimed, coord, fn
+              nil ->
+                {[], [id]}
+
+              conflicts ->
+                new_conflicts = [id | conflicts]
+                {new_conflicts, new_conflicts}
+            end)
         end)
 
-      # Map.update(claimed, )
-      # Need to maintain a list of valid ids (or a list of invalid ids)
+      # now we have conflicting_ids and an updated claimed map
+      valid_ids =
+        conflicting_ids
+        |> Enum.reduce(result.valid_ids, fn conflicting_id, valid_ids ->
+          MapSet.delete(valid_ids, conflicting_id)
+        end)
+
+      %{result | valid_ids: valid_ids, claimed: claimed}
     end)
   end
 end
