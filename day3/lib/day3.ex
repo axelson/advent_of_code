e98426148f5bbd314587aed2933274278e659f25
Jason Axelson <jason.axelson@gmail.com>
2018-12-04 20:29:48 -1000

    Initial working day3 version

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -24,30 +24,29 @@ defmodule Day3 do
 
   def part2() do
     claims =
-      # Advent.input("short.txt")
       Advent.input("day3_input.txt")
       |> String.split("\n", trim: true)
       |> Enum.map(&Claim.parse/1)
 
-    maybe_valid_ids =
+    all_ids =
       claims
       |> Enum.map(& &1.id)
       |> MapSet.new()
 
     claims
     |> Enum.map(&{&1.id, Claim.coords(&1)})
-    |> Enum.reduce(%Result{claimed: %{}, valid_ids: maybe_valid_ids}, fn {id, coords}, result ->
+    |> Enum.reduce(%Result{claimed: %{}, valid_ids: all_ids}, fn {id, coords}, result ->
       {conflicting_ids, claimed} =
-        Enum.reduce(coords, {[], result.claimed}, fn coord, {conflicting_ids, claimed} ->
-          {conflicts, claimed} =
-            Map.get_and_update(claimed, coord, fn
-              nil ->
-                {[], [id]}
+        Enum.reduce(coords, {MapSet.new(), result.claimed}, fn coord, {conflicting_ids, claimed} ->
+          Map.get_and_update(claimed, coord, fn
+            nil ->
+              {conflicting_ids, [id]}
 
-              conflicts ->
-                new_conflicts = [id | conflicts]
-                {new_conflicts, new_conflicts}
-            end)
+            conflicts ->
+              new_conflicts = [id | conflicts]
+              conflicting_ids = MapSet.union(conflicting_ids, MapSet.new(new_conflicts))
+              {conflicting_ids, new_conflicts}
+          end)
         end)
 
       # now we have conflicting_ids and an updated claimed map
