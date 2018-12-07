4e651cd7a6a4ae2c4959fa79f61a5a874bf89efb
Jason Axelson <jason.axelson@gmail.com>
2018-12-06 21:50:41 -1000

    Clean up day3 with Integer.parse

--- day3/lib/claim.ex
+++ day3/lib/claim.ex
@@ -1,25 +1,20 @@
 defmodule Day3.Claim do
   defstruct [:id, :x, :y, :width, :height]
 
-  @digits ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
   def parse("#" <> rest) do
-    rest = String.codepoints(rest)
+    {id, rest} = Integer.parse(rest)
+    <<" @ ", rest::binary>> = rest
 
-    {id, rest} = parse_digit(rest)
+    {x, rest} = Integer.parse(rest)
+    <<",", rest::binary>> = rest
+    {y, rest} = Integer.parse(rest)
 
-    [" ", "@", " " | rest] = rest
+    <<": ", rest::binary>> = rest
 
-    {x, rest} = parse_digit(rest)
-    ["," | rest] = rest
-    {y, rest} = parse_digit(rest)
-
-    [":", " " | rest] = rest
-
-    {width, rest} = parse_digit(rest)
-    ["x" | rest] = rest
-    {height, rest} = parse_digit(rest)
-    [] = rest
+    {width, rest} = Integer.parse(rest)
+    <<"x", rest::binary>> = rest
+    {height, rest} = Integer.parse(rest)
+    "" = rest
 
     %__MODULE__{
       id: id,
@@ -28,20 +23,6 @@ defmodule Day3.Claim do
       width: width,
       height: height
     }
-  end
-
-  defp parse_digit(rest) do
-    {digits, rest} = do_parse_digit(rest)
-    {Enum.join(digits) |> String.to_integer(), rest}
-  end
-
-  defp do_parse_digit([digit | rest]) when digit in @digits do
-    {digits, rest} = do_parse_digit(rest)
-    {[digit | digits], rest}
-  end
-
-  defp do_parse_digit(rest) do
-    {[], rest}
   end
 
   @doc """
