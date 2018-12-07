fce2cd02b6df1ddaef02c03f1a7cd96db6479e81
Jason Axelson <jason.axelson@gmail.com>
2018-12-06 22:20:49 -1000

    Finish day5 part1

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -1,18 +1,47 @@
 defmodule Day5 do
-  @moduledoc """
-  Documentation for Day5.
-  """
+  @case_difference ?a - ?A
 
-  @doc """
-  Hello world.
+  def part1 do
+    # file = "short.txt"
+    file = "day5_input.txt"
 
-  ## Examples
+    input(file)
+    |> reduce()
+    |> String.length()
+  end
 
-      iex> Day5.hello()
-      :world
+  def reduce(input) do
+    input
+    |> :erlang.binary_to_list()
+    |> do_reduce()
+    |> IO.iodata_to_binary()
+  end
 
-  """
-  def hello do
-    :world
+  defp do_reduce(input) do
+    case reduce_input(input) do
+      ^input -> input
+      reduced -> do_reduce(reduced)
+    end
+  end
+
+  defp reduce_input([]), do: []
+  defp reduce_input([char]), do: [char]
+  defp reduce_input([char1, char2 | rest]) do
+    if opposite_polarities?(char1, char2) do
+      reduce_input(rest)
+    else
+      [char1 | reduce_input([char2 | rest])]
+    end
+  end
+
+  def input(file) do
+    {:ok, input} = File.read(file)
+    String.trim(input)
+  end
+
+  # Two characters are opposite polarities if they are the same letter but one
+  # is uppercase and one is lower case
+  defp opposite_polarities?(char1, char2) do
+    abs(char1 - char2) == @case_difference
   end
 end
--- day5/test/day5_test.exs
+++ day5/test/day5_test.exs
@@ -2,7 +2,21 @@ defmodule Day5Test do
   use ExUnit.Case
   doctest Day5
 
-  test "greets the world" do
-    assert Day5.hello() == :world
+  test "parses" do
+    input = "dabAcCaCBAcCcaDA"
+    expected = "dabCBAcaDA"
+    assert Day5.reduce(input) == expected
+  end
+
+  test "parses 2" do
+    input = "abBA"
+    expected = ""
+    assert Day5.reduce(input) == expected
+  end
+
+  test "reduces out of order" do
+    input = "ABba"
+    expected = ""
+    assert Day5.reduce(input) == expected
   end
 end
