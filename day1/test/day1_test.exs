8642ab7eace1b2a32e7ce3c9868b5864b867a525
Jason Axelson <jason.axelson@gmail.com>
2019-12-07 09:26:26 -1000

    Finish day1 part2

--- day1/test/day1_test.exs
+++ day1/test/day1_test.exs
@@ -12,4 +12,14 @@ defmodule Day1Test do
       assert required_fuel(100_756) == 33583
     end
   end
+
+  describe "total_required_fuel/1" do
+    import Day1, only: [total_required_fuel: 1]
+
+    test "sample values" do
+      assert total_required_fuel(14) == 2
+      assert total_required_fuel(1969) == 966
+      assert total_required_fuel(100_756) == 50346
+    end
+  end
 end
