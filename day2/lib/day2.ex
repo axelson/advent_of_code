8c227532c290fb67dc7606a915b23760c2d11b31
Jason Axelson <jason.axelson@gmail.com>
2019-12-07 13:35:33 -1000

    Finish day 2 part 2

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -14,6 +14,40 @@ defmodule Day2 do
     |> hd()
   end
 
+  def part2 do
+    prog =
+      Advent.comma_input(@file_path)
+      |> Enum.map(&String.to_integer/1)
+      |> Prog.intcodes_to_prog()
+
+    expected_output = 19_690_720
+
+    for noun <- 0..99, verb <- 0..99, into: %{} do
+      prog = restore_noun_verb(prog, noun, verb)
+      output = produce_output(prog)
+
+      if output == expected_output do
+        answer = 100 * noun + verb
+        {:ok, answer}
+      else
+        {:error, :skip}
+      end
+    end
+    |> Map.get(:ok)
+  end
+
+  def produce_output(prog) do
+    Prog.execute_prog(prog)
+    |> Prog.to_intcodes()
+    |> hd()
+  end
+
+  def restore_noun_verb(prog, noun, verb) do
+    prog
+    |> Prog.store_pos(1, noun)
+    |> Prog.store_pos(2, verb)
+  end
+
   def restore_gravity_assist_program(prog) do
     # replace position 1 with the value 12
     prog = Prog.store_pos(prog, 1, 12)
--- day2/test/day2_test.exs
+++ day2/test/day2_test.exs
@@ -12,4 +12,12 @@ defmodule Day2Test do
       assert execute_intcodes([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
     end
   end
+
+  test "part 1 answer" do
+    assert Day2.part1() == 5_434_663
+  end
+
+  test "part 2 answer" do
+    assert Day2.part2() == 4559
+  end
 end
