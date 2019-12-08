7723debd2f7a97004ff36b15f8c3081feae4c5ea
Jason Axelson <jason.axelson@gmail.com>
2019-12-08 06:58:45 -1000

    Day8 part1

--- day8/lib/day8.ex
+++ day8/lib/day8.ex
@@ -1,18 +1,42 @@
 defmodule Day8 do
-  @moduledoc """
-  Documentation for Day8.
-  """
+  @file_path Path.join(__DIR__, "input")
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day8.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def part1(width \\ 25, height \\ 6) do
+    Advent.input(@file_path)
+    |> String.trim()
+    |> corruption_check(width, height)
   end
+
+  def corruption_check(input, width, height) do
+    image = parse_lines(input, width, height)
+
+    layer =
+      Enum.min_by(image, fn layer ->
+        count_digits(layer, 0)
+      end)
+
+    count_digits(layer, 1) * count_digits(layer, 2)
+  end
+
+  def parse_lines(input, width, height) do
+    input
+    |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(width * height)
+    |> Enum.map(fn layer ->
+      Enum.chunk_every(layer, width)
+    end)
+  end
+
+  def count_digits([], _digit), do: 0
+
+  def count_digits([list | rest], digit) when is_list(list) do
+    count_digits(list, digit) + count_digits(rest, digit)
+  end
+
+  def count_digits([digit | rest], digit) do
+    1 + count_digits(rest, digit)
+  end
+
+  def count_digits([_ | rest], digit), do: count_digits(rest, digit)
 end
--- day8/test/day8_test.exs
+++ day8/test/day8_test.exs
@@ -1,8 +1,39 @@
 defmodule Day8Test do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Day8
 
-  test "greets the world" do
-    assert Day8.hello() == :world
+  describe "corruption_check/1" do
+    import Day8, only: [corruption_check: 3]
+
+    test "sample values" do
+      assert corruption_check("123456789012", 3, 2) == 1
+      assert corruption_check("123451789012", 3, 2) == 2
+      assert corruption_check("123411789012", 3, 2) == 3
+      assert corruption_check("123211789012", 3, 2) == 6
+    end
   end
+
+  test "part 1 answer" do
+    assert Day8.part1() == 2064
+  end
+
+  # describe "part2_distance/2" do
+  #   import Day8, only: [part2_distance: 1]
+
+  #   test "sample values" do
+  #     assert part2_distance("""
+  #            R75,D30,R83,U83,L12,D49,R71,U7,L72
+  #            U62,R66,U55,R34,D71,R55,D58,R83
+  #            """) == 610
+
+  #     assert part2_distance("""
+  #            R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+  #            U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+  #            """) == 410
+  #   end
+  # end
+
+  # test "part 2 answer" do
+  #   assert Day8.part2() == 28580
+  # end
 end
