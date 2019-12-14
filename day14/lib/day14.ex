8464bd747946e1eb08db89be41aff1f80b6f8662
Jason Axelson <jason.axelson@gmail.com>
2019-12-14 12:33:43 -1000

    Day 14 skeleton

---
+++ day14/lib/day14.ex
@@ -0,0 +1,3 @@
+defmodule Day14 do
+  @file_path Path.join(__DIR__, "input")
+end
---
+++ day14/test/day14_test.exs
@@ -0,0 +1,7 @@
+defmodule Day14Test do
+  use ExUnit.Case, async: true
+  doctest Day14
+
+  test "sample values" do
+  end
+end
